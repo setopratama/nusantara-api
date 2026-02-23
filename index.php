a8325fe234333639a3c9d3ff7bc0b4a802f25399
setopratama <setopratama@gmail.com>
2026-02-23 08:12:52 +0700

    Update response font to IBM Plex Mono for Postman-like experience

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <title>Nusantara API Documentation - Postman Clone</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Fira+Code:wght@400;500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=IBM+Plex+Mono:wght@400;500;600&family=Fira+Code:wght@400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/modern-ui.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
